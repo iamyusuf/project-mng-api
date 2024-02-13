2bc95000836b030441bd3b268a38dd039cfbfff7
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-02-13 14:31:25 +0600

    feat: add a method fiber_to_coroutine to convert a fiber to a coroutine in helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,33 @@
 <?php
+
+function fiber_to_coroutine(\Fiber $fiber): \Generator
+{
+    $index = -1; // Note: Pre-increment is faster than post-increment
+    $value = null;
+
+    // allow an already running fiber
+    if (!$fiber->isStarted()) {
+        $value = yield ++$index => $fiber->start();
+    }
+
+    // A Fiber without suspends should return the result immediately.
+    if (!$fiber->isTerminated()) {
+        while (true) {
+            $value = $fiber->resume($value);
+
+            // The last call to "resume()" moves the execution of the
+            // Fiber to the "return" stmt.
+            //
+            // So the "yield" is not needed. Skip this step and return
+            // the result.
+
+            if ($fiber->isTerminated()) {
+                break;
+            }
+
+            $value = yield ++$index => $value;
+        }
+    }
+
+    return $fiber->getReturn();
+}

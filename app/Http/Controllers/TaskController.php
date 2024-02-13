c51f8f62904e07d39867bc4a82f33f1c9414bb57
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-02-13 16:55:27 +0600

    feat: create a crud for task

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TaskController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Task::query();
+        $this->addTaskFilters($query, $request);
+        $data = $query->paginate();
+
+        return response()->json($data);
+    }
+
+    public function create(Request $request)
+    {
+        $task = new Task($request->all());
+        $task->save();
+        return response()->json($task, Response::HTTP_CREATED);
+    }
+
+    public function update(Task $task, Request $request)
+    {
+        $task->fill($request->all());
+        $task->save();
+
+        return response()->json($task, Response::HTTP_OK);
+    }
+
+    public function delete(Task $task)
+    {
+        $id = $task->getKey();
+        $task->delete();
+        return response()->json(['id' => $id], Response::HTTP_NO_CONTENT);
+    }
+
+    public function view(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    private function addTaskFilters(Builder $query, Request $request)
+    {
+        $queryableFields = ['project_id', 'epic_id', 'story_id'];
+
+        foreach ($queryableFields as $field) {
+            $query->where($request->has($field), function ($q) use ($request, $field) {
+                $q->where($field, $request->input($field));
+            });
+        }
+    }
+}

2c5c2e44e7ed6da05f901ab5b206863f97f0da5f
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-02-13 14:32:11 +0600

    feat: create an enum to learn how enum works in php

--- app/Enums/Suit.php
+++ app/Enums/Suit.php
@@ -2,7 +2,36 @@
 
 namespace App\Enums;
 
-enum Suit
+enum Suit: string
 {
+    case Hearts = "H";
+    case Diamonds = "D";
+    case Clubs = "C";
+    case Spades = "S";
 
+    public function color(): string
+    {
+        return match($this) {
+            Suit::Hearts, Suit::Diamonds => 'Red',
+            Suit::Clubs, Suit::Spades => 'Black'
+        };
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    public static function forSelect(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
+
+    public static function forSelect2(): array
+    {
+        return array_combine(
+            array_column(self::cases(), 'value'),
+            array_column(self::cases(), 'name')
+        );
+    }
 }
